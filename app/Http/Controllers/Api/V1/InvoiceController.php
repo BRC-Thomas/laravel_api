b12aede2607dd19cbc9cd6d91469ef926a91f279
BRC-Thomas <thomasbaracani@gmail.com>
2024-04-04 14:56:40 +0200

    json format

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
-use App\Http\Controllers\Controler;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
 
 
 class InvoiceController extends Controller
@@ -15,7 +17,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        //
+        return new InvoiceCollection(Invoice::paginate());
     }
 
     /**
@@ -23,7 +25,7 @@ class InvoiceController extends Controller
      */
     public function create()
     {
-        //
+
     }
 
     /**
@@ -39,7 +41,7 @@ class InvoiceController extends Controller
      */
     public function show(Invoice $invoice)
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
