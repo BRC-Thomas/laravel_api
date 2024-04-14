d6e45ef70b49fc0b91d4debfe38fc9a5802f3b25
BRC-Thomas <thomasbaracani@gmail.com>
2024-04-14 08:52:11 +0200

    including related data

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -20,17 +20,17 @@ class CustomerController extends Controller
     public function index(Request $request)
     {
       $filter = new CustomersFilter();
-      $queryItems = $filter->transform($request); //['column', 'operator', 'value']]
+      $filterItems = $filter->transform($request); //['column', 'operator', 'value']]
 
-      if(count($queryItems) == 0){
-          return new CustomerCollection(Customer::paginate());
-      } else {
-        $customers = Customer::where($queryItems)->paginate();
-        return new CustomerCollection($customers->appends($request->query()));
+      $includeInvoices = $request->query('includeInvoices');
 
+      $customers = Customer::where($filterItems);
+
+      if($includeInvoices){
+        $customers = $customers->with('invoices');
       }
-
-      Customer::where($queryItems);
+      
+      return new CustomerCollection($customers->paginate()->appends($request->query()));
 
 
     }
@@ -56,7 +56,16 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        return new CustomerResource($customer);
+
+      $includeInvoices = request()->query('includeInvoices');
+
+      if($includeInvoices){
+        return new CustomerResource($customer->loadMissing('invoices'));
+
+      }
+
+      return new CustomerResource($customer);
+
     }
 
     /**
